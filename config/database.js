fa1364fb8899271fb818bd236668f4e458f7db66
bilalabdelkadir <bilalbinabdelkadir@gmail.com>
2023-06-13 23:10:48 +0300

    feat: database config

---
+++ config/database.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+// this will connect the database and let us login
+mongoose.set("strictQuery", false);
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URL)
+    .then(() => console.log("database conneted"))
+    .catch((error) =>
+      console.log(`failed to connect [[[[----${error} ----]]]]`)
+    );
+};
+
+export default connectDatabase;
